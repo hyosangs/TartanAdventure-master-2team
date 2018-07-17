22772570cb9680ffb0037940f40225607f325e98
hyosangs <hyosang.shin@lge.com>
2018-07-17 09:34:14 +0900

    Remove this unnecessary cast to "Inspectable".
    #223

--- src/edu/cmu/tartan/Game.java
+++ src/edu/cmu/tartan/Game.java
@@ -205,7 +205,7 @@ public class Game {
 				Item item = a.directObject();
 				if (this.player.currentRoom().hasItem(item) || this.player.hasItem(item)) {
 					if (item instanceof Inspectable) {
-						((Inspectable) item).inspect();
+						(item).inspect();
 					} else {
 						System.out.println("You cannot inspect this item.");
 					}
