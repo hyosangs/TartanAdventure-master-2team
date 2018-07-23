473bf1cd4e548105e11f1116eaa37f6cfab6cb03
kiwan <menathe@naver.com>
2018-07-22 20:50:42 -0400

    [UNIT TEST] Unit test code for Action.Java

--- tests/edu/cmu/tartan/action/ActionTest.java
+++ tests/edu/cmu/tartan/action/ActionTest.java
@@ -1,6 +1,5 @@
 package edu.cmu.tartan.action;
 
-import edu.cmu.tartan.Game;
 import edu.cmu.tartan.Player;
 import edu.cmu.tartan.PlayerInterpreter;
 import edu.cmu.tartan.item.Item;
@@ -12,6 +11,60 @@ import static org.junit.Assert.*;
 public class ActionTest {
 
     @Test
+    public void getAliasesTest() {
+        // given
+        Action action = Action.ACTION_LOOK;
+        // when
+        String[] aliases = action.getAliases();
+        // then
+        assertEquals("lookAround", aliases[0]);
+    }
+
+    @Test
+    public void typeTest() {
+        // given
+        Action action = Action.ACTION_LOOK;
+        // when
+        Type type = action.type();
+        // then
+        assertEquals(Type.TYPE_HASNOOBJECT, type);
+    }
+
+    @Test
+    public void setDirectObjectTest() {
+        // given
+        Action action = Action.ACTION_PICK_UP;
+        Item item = Item.getInstance("gold");
+        // when
+        action.setDirectObject(item);
+        // then
+        assertEquals(Item.getInstance("gold"), item);
+    }
+
+    @Test
+    public void directObjectTest() {
+        // given
+        Action action = Action.ACTION_PICK_UP;
+        Item item = Item.getInstance("gold");
+        action.setDirectObject(item);
+        // when
+        item = action.directObject();
+        // then
+        assertEquals(Item.getInstance("gold"), item);
+    }
+
+    @Test
+    public void setIndirectObjectTest() {
+        // given
+        Action action = Action.ACTION_TAKE;
+        Item item = Item.getInstance("microwave");
+        // when
+        action.setIndirectObject(item);
+        // then
+        assertEquals(Item.getInstance("microwave"), item);
+    }
+
+    @Test
     public void indirectObjectTest() {
         // given
         Action action = Action.ACTION_TAKE;
@@ -21,6 +74,26 @@ public class ActionTest {
         item = action.indirectObject();
         // then
         assertEquals(Item.getInstance("microwave"), item);
+    }
+
+    @Test
+    public void getOppositeDirectionTest() {
+        // given
+        Action action = Action.ACTION_GO_EAST;
+        // when
+        Action opposite = action.getOppositeDirection();
+        // Then
+        assertEquals(Action.ACTION_GO_WEST, opposite);
+    }
+
+    @Test
+    public void getOppositeDirectionNotDirectionTypeTest() {
+        // given
+        Action action = Action.ACTION_PASS;
+        // when
+        Action opposite = action.getOppositeDirection();
+        // Then
+        assertEquals(null, opposite);
     }
 
     @Test
@@ -106,7 +179,16 @@ public class ActionTest {
 
     @Test
     public void actionLook() {
-
+        Room room = new Room();
+        Room newRoom = new Room("Test room", "Test room");
+        room.player = new Player(room);
+        Action action = Action.ACTION_LOOK;
+        Item item = Item.getInstance("key");
+        action.setIndirectObject(item);
+        room.player.currentRoom().putItem(item);
+        room.setAdjacentRoom(action, newRoom);
+        action.actionLook(room.player);
+        //
     }
 
     @Test
@@ -115,6 +197,7 @@ public class ActionTest {
 
     @Test
     public void actionPut() {
+
     }
 
     @Test
